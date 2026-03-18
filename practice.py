d5061412b7b4419bd3311529df71f290e23deef8
TanishGamare <tanishgamare20220@gmail.com>
2026-03-18 16:46:06 +0530

    Modified practice.py

--- practice.py
+++ practice.py
@@ -1,5 +1,14 @@
-first_name = "Tanish "
-print(f"Hello {first_name}")
+age = int(input("Enter your age") ) 
+if age >= 50:
+    print("You're old get a job")
+else:
+    print("yea please get a job")
+ 
+  
+ 
 
-age = 19
-print(f"You are {age} years old")
+
+
+
+
+
