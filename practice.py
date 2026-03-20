98ecdf095bd6ba3c9bc2f816387a5949a3dfe95c
TanishGamare <tanishgamare20220@gmail.com>
2026-03-20 12:06:15 +0530

    Maths module python

--- practice.py
+++ practice.py
@@ -1,8 +1,29 @@
-age = int(input("Enter your age") ) 
-if age >= 50:
-    print("You're old get a job")
-else:
-    print("yea please get a job")
+import math 
+
+radius = float(input("Enter a radius for circle: "))
+
+Area  = math.pi * radius 
+print(f"The area of the circle is {Area}")
+
+
+
+
+
+
+
+   
+
+
+
+
+
+
+
+    
+
+    
+
+
  
   
  
